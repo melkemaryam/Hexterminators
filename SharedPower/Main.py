f06d895b135852a5f2e5b746ad379cfd7f42fea3
melkemaryam <31968055+melkemaryam@users.noreply.github.com>
2019-12-02 19:59:48 +0000

    uploaded the whole folder with the classes

---
+++ SharedPower/Main.py
@@ -0,0 +1,13 @@
+#Explanation:
+
+#Main: The class where all the different functions are put together in a logical order, only with "function()", nothing more
+
+#I will divide the whole interface code into the different classes, so we can actually start coding with logical functions
+
+#basic things to use:
+
+#raw_input is the same as input(), the only difference is: raw_input was for string inputs in Python, but  that was Python version 2.x
+#-> it is better to use only input(), I was just so used to raw_input
+
+#AND: Please your variable names wisely, so everyone knows what they mean
+#AND: Please write comments describing what the function does: #comments
