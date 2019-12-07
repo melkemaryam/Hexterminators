88b0e0c0516e184af7530f9ac1d0dc54a7ad3cfd
worazz <piotr.woroszyllo@gmail.com>
2019-12-07 22:30:54 +0000

    'too make things easier for myself I used Greg's notes, since I really couldn't get to the bottom of working on dataframes (for now - this could be done as csmetics ones we have working version)

--- SharedPower/Invoice.py
+++ SharedPower/Invoice.py
@@ -1,29 +1,67 @@
-import pandas as pd
-
-import numpy as np
-
-
-def load_data(path):
-    # loads dataframe and returns it 
-    # change 'csv' with whatever type the data frame is stored as    
-
-    dataframe = pd.read_csv(path)
-    return dataframe
+import sqlite3
 
 class Invoice:
+    def __init__(self, invoice_id):
+        self.invoice_id = invoice_id
 
-    def count_month(name):
-        monies = dataframe.groupby(["customer_ID"==name]).sum(numeric_only=bool)
-        #combines all invoices of a single customer
+    def getData(database_filename):
+        theCustomer = None
+        database_connection = sqlite3.connect(database_filename)
+        #estabilishing a db connection
 
-        return monies
-    
+        cursor = database_connection.cursor()
+        #cursor creation for talking to db
 
+        cursor.execute('SELECT customer_ID, customer_forename, customer_surname, customer_email FROM customer WHERE customer_email = ?'), (customer_email,))
+        customer_row=cursor.fetchone()
+        if (customer_row != None):
+            #checking if customer with given email exists (if doesn't will return 'None')
 
-##eliminate multiple entries of same ID
-##if monies==0 generate email "we realised you didn't rent any tools this month, are all your project now done?"
-=======
-    print("Hello")
-    print("Hi test")
-    print("Hi again")
->>>>>>> 63b84d8bab353861d0dd5f7d8615361e6ed76d92
+            customer_id = customer_row[0]
+            customer_forename = customer_row[1]
+            customer_surname = customer_row[2]
+            customer_email = customer_row[3]
+            theCustomer = Customer (customer_id, customer_forename, customer_surname, customer_email)
+            #customer creation based on retrieved data
+
+        database_connection.close()
+        #close db conection to free resources
+
+        return theCustomer
+        #presenting the Customer back to us
+
+    def getTool(database_filename)
+        rental.list = []
+        database_connection = sqlite3.connect(database_filename)
+        #estabilishing a db connection
+
+        cursor = database_connection.cursor()
+        #cursor creation for talking to db
+
+        cursor.execute('SELECT tool_ID, tool_name, price, duration, customer_id FROM tool WHERE customer_id= ?'), (customer_id,))
+        tool_row=cursor.fetchall()
+        for tool in tool_rows:
+            #checking if customer with given id borrowed any tools and if it was more than one creating a list
+
+            customer_id = tool[0]
+            tool_name = tool[1]
+            price = tool[2]
+            duration = tool[3]
+            total_price = int(price) * int(duration)
+            theTool = Tool (customer_id, tool_name, price, duration, total_price)
+            rentalList.append(theTool)
+            #rental lines creation based on retrieved data
+
+        database_connection.close()
+        #close db conection to free resources
+
+        return rental.list
+        #presenting the Rental lines back to us
+            
+import smtplib
+# import the smtplib module. It should be included in Python by default
+
+s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
+s.starttls()
+s.login(MY_ADDRESS, PASSWORD)
+# set up the SMTP server as we use outlook 365
