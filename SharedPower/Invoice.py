e0654829f27fa349839dfde8fa64fa7913e69551
worazz <piotr.woroszyllo@gmail.com>
2019-12-15 22:48:03 +0000

    almost there

--- SharedPower/Invoice.py
+++ SharedPower/Invoice.py
@@ -1,6 +1,15 @@
 import sqlite3
+#for talking to the database
+import numpy
 import pandas
+#for tables and making them fancy
 from datetime import datetime
+#for checking the time
+import smtplib
+#import the smtplib module for setting up mail connection. It should be included in Python by default
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+#more emailing from python stuff
 
 class Invoice:
     def __init__(self, invoice_id, customer_id, customer_forename, customer_surname, customer_email, tool_ID, tool_name, price, duration):
@@ -13,7 +22,9 @@ class Invoice:
         self.price = price
         self.duration = duration
 
-    def getData(self, database_filename):
+    def getData(self, customer_email, database_filename):
+        #for finding customer in the base using email address
+
         database_connection = sqlite3.connect(database_filename)
         #estabilishing a db connection
 
@@ -25,7 +36,6 @@ class Invoice:
         if (customer_row != None):
             #checking if customer with given email exists (if doesn't will return 'None')
 
-            customer_id = customer_row[0]
             customer_forename = customer_row[1]
             customer_surname = customer_row[2]
             customer_email = customer_row[3]
@@ -34,11 +44,14 @@ class Invoice:
         database_connection.close()
         #close db conection to free resources
 
-        return customer_id, customer_forename, customer_surname, customer_email
+        return customer_forename, customer_surname, customer_email
         #presenting the Customer back to us
 
-    def getTool(self, database_filename):
+    def getTool(self, customer_id, database_filename):
+        #for getting all the tools used by customer
+
         rental_list = ['Tool Name', 'Day Price', 'Rental Duration', 'Total Price']
+        grand_total = 0
         database_connection = sqlite3.connect(database_filename)
         #estabilishing a db connection
 
@@ -55,42 +68,67 @@ class Invoice:
             price = tool[2]
             duration = tool[3]
             total_price = int(price) * int(duration)
+            grand_total = grand_total + total_price
             rental_list.append(tool_name, price, duration, total_price)
             #rental lines creation based on retrieved data
 
         database_connection.close()
         #close db conection to free resources
-        return rental_list
+        return rental_list, grand_total
         #presenting the Rental lines back to us
     
-    def cut_list(self):
-        items = len(rental_list)
-        rows = items/4
-        for x_i in range(rows):
-            yield [rental_list[y_i] for y_i in range(x_i, items, rows)]
-        #rearranging single list into list of lists for each line of the invoice to be generated
+    def cut_list(self, rental_list):
+        #rearranging single list from getTool function into a table for the invoice to be generated
     
-    def generate_invoice(self):
-        invoice_main_body = pandas.DataFrame(rental_list, columns=headers)
-        #returns list of lists as table (should be neat)
-        grand_total = invoice_main_body['Total Price'].sum
-        #collects all the single total prices from subsequent lines
+        tool_droppings = numpy.asarray(rental_list)
+        invoice_table = numpy.reshape(tool_droppings, (-1,4))
+        return invoice_table
+        
+    def generate_invoice(self, rental_list, grand_total, invoice_table, customer_forename, customer_surname):
+        #building the actual invoice - message and numbers together
+
+        invoice_main_body = pandas.DataFrame(invoice_table, columns=['Tool Name', 'Day Price', 'Rental Duration', 'Total Price'])
+        #adding headers to numpy array to make it more table-like (should be neat)
         name = customer_forename + ' ' + customer_surname
         period = str(datetime.now().month) + ' ' + str(datetime.now().year)
         customer_info = (name, period, invoice_main_body, grand_total)
-        format_invoice ="""Hello %s,
+        format_invoice = ("""Hello %s,
         Please find your invoice for %s below: 
         %s
         Totaling at: %s
-        Thank you for using SharedPower."""
-        return (format_invoice % customer_info)
-        #e voila!
-#now for sending the bitch away >.<
-            
-import smtplib
-# import the smtplib module. It should be included in Python by default
 
-s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
-s.starttls()
-s.login(MY_ADDRESS, PASSWORD)
-# set up the SMTP server as we use outlook 365
+        Thank you for using SharedPower.""" % customer_info)
+        return format_invoice
+        #e voila!
+    
+    def send_invoice(self, invoice_table, customer_forename, customer_surname, customer_email, format_invoice, customer_info):
+        #now for sending the bitch away >.<
+
+        MY_ADDRESS = 'breo.Piotr.Hexterminators@study.beds.ac.uk'
+        PASSWORD = 'need.to.hash.this.away'
+        #give it access to basic SharedPower customer services email address
+
+        s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
+        s.starttls()
+        s.login(MY_ADDRESS, PASSWORD)
+        #set up the SMTP server as we use outlook 365
+
+        msg = MIMEMultipart()       #create a message
+
+        message = Invoice.generate_invoice(invoice_table, customer_forename, customer_surname)
+
+        msg['From']=MY_ADDRESS
+        msg['To']= customer_email
+        msg['Subject']="This is your monthly SharedPower invoice"
+        #setup the parameters of the message
+
+        msg.attach(MIMEText(message, 'plain'))
+        #add in the message body
+
+        s.send_message(msg)
+        #send the message via the server set up earlier.
+    
+        del msg
+
+        s.quit()
+        #clean up done
