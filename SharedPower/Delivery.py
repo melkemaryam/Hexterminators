2bd9d968e5c6a4c69e004a7dfc1b48fecfb0737e
worazz <57625531+worazz@users.noreply.github.com>
2020-01-08 12:39:09 +0000

    Update Delivery.py

--- SharedPower/Delivery.py
+++ SharedPower/Delivery.py
@@ -1,6 +1,14 @@
 import sqlite3
-
+from DatabaseConnection import DatabaseConnection
 
 class Delivery:
-    print("Hello")
-    print("test")
+    def __init__(self):
+        self.booking_id = booking_id
+    
+    def add_delivery_charge(self, booking_id):
+        DatabaseConnection.CreateDBconnection(database_filename)
+        
+        #add 1 value to delivery column in the corresponding booking ID
+        
+        DatabaseConnection.CloseConnection(database_filename)
+        
