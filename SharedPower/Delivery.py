90155875b340fc492fb6cc8f30cce87c8013cafb
worazz <57625531+worazz@users.noreply.github.com>
2020-01-08 12:53:56 +0000

    Update Delivery.py

--- SharedPower/Delivery.py
+++ SharedPower/Delivery.py
@@ -8,7 +8,10 @@ class Delivery:
     def add_delivery_charge(self, booking_id):
         DatabaseConnection.CreateDBconnection(database_filename)
         
-        #add 1 value to delivery column in the corresponding booking ID
+        cursor = database_connection.cursor()
+        #cursor creation for talking to db
+
+        cursor.execute('UPDATE Tool SET delivery = delivery + 1 WHERE booking_id = ?', booking_id )
         
         DatabaseConnection.CloseConnection(database_filename)
         
