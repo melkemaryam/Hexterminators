2caaa9707460a78c353110e0e6160ca4e4a1bfae
Piotr Woroszyllo <1908085@beds.ac.uk>
2020-01-13 12:06:51 +0000

    multidays availability done

--- SharedPower/LateCharge.py
+++ SharedPower/LateCharge.py
@@ -19,21 +19,25 @@ class LateCharge:
 
     def __init__(self, return_date, charge_ratio,):
         self.return_date = return_date
-        self.charge_ratio = charge_ratio
+        self.charge_ratio = 2
+        #basic late charge is double the rate
 
     def checkIfLate(self, return_date, bookingIdReturnInput):
         return_date = date.today()
         actual_booking = []
         DatabaseConnection.CreateDBConnection()
         
-        cursor.execute('SELECT date FROM bookings WHERE booking_id = ?', bookingIdReturnInput)
+        cursor.execute('SELECT date FROM booking WHERE booking_id = ?', bookingIdReturnInput)
         booking_dates = cursor.fetchall()
             for date in booking_dates:
                 actual_booking.append(booking_dates)
+        #checks which dates belong to the booking
 
         late_days = (return_date - max(actual_booking)).days
+        #counts how many days overdue
         late_charge = charge_ratio * late_days
+        #adds the ratio for later calculation
 
-        cursor.execute('UPDATE bookings SET late = ? WHERE booking_id = ?', late_charge, bookingIdReturnInput)
+        cursor.execute('UPDATE booking SET late_return = ? WHERE booking_id = ?', late_charge, bookingIdReturnInput)
 
         DatabaseConnection.CloseDBConnection()
