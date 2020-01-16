3d03a85235790eae193cfc2fefe96bea83117470
Hannah <Hannah@pc-142-20.customer.ask4.lan>
2020-01-16 18:02:18 +0000

    added Cornelias test file

---
+++ SharedPower/BookingManagerTests.py
@@ -0,0 +1,44 @@
+''' 
+-------------------------------------------------
+
+Project: SharedPower
+Group: Hexterminators
+
+File name: BookingManagerTests.py
+
+Created: 16th January 2020
+-------------------------------------------------
+'''
+
+from sqlite3 import Error
+
+from ToolManager import ToolManager
+from BookingManager import BookingManager
+
+from Tools import Tools
+from Bookings import Bookings
+
+class BookingManagerTests:
+
+    # Test the Search Future Bookings 
+    def TestSearchFutureBookings(self, database_path):
+
+        function_name   = 'TestSearchFutureBookings'
+
+        try:
+
+            # Get a tool from the tool manager
+            toolManager = ToolManager(database_path)
+            test_tool = toolManager.loadToolId(1)
+
+            bookingManager = BookingManager(database_path)
+
+            futureBookings = bookingManager.searchFutureBookings(test_tool)
+
+            for booking in futureBookings:
+                print(booking)
+
+        except Error as e:
+
+            # Catch and display any errors that occur
+            print(__name__, ':', function_name, " - ", e)
